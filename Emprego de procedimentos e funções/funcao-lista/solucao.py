7366dd2a97a58b7234a9f2d91db71e8efceed7b9
Abner Valverde Cunha <abnercunha2247@gmail.com>
2023-10-18 11:24:23 -0300

    update all files and update function1

---
+++ Emprego de procedimentos e funções/funcao-lista/solucao.py
@@ -0,0 +1,10 @@
+def encontrar_minimo(lista):
+    minimo = lista[0]
+    for elem in lista:
+        if(elem < minimo):
+            minimo = elem
+    return minimo
+
+lista_teste = [2,10,3,1,4,5]
+menor = encontrar_minimo(lista_teste)
+print("O menor elemento da lista é :[{}]".format(menor))
