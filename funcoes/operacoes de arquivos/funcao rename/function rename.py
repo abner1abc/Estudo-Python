f05b5ca3d3cc386bd3dc05f78a4f02dec3c706a6
Abner Valverde Cunha <abnercunha2247@gmail.com>
2024-05-28 17:31:16 -0300

    update new code function

---
+++ funcoes/operacoes de arquivos/funcao rename/function rename.py
@@ -0,0 +1,10 @@
+import os
+nome_antigo = "arquivo_antigo.txt"
+nome_novo = "arquivo_novo.txt"
+try:
+    os.rename(nome_antigo, nome_novo)
+    print(f"O arquivo {nome_antigo} foi renomeado para {nome_novo}.")
+except FileNotFoundError:
+    print(f"O arquivo {nome_antigo} não foi encontrado.")
+except Exception as e: 
+    print(f"Ocorreu um erro ao renomear o arquivo: {e}")
